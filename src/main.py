107409cf2c4317619b92bc741a05f29b67db8b15
Xinyang Li <xinyang.li@tum.de>
2023-09-21 16:38:05 +0200

    add precommit

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import autorootcwd
 import hydra
-from omegaconf import DictConfig, OmegaConf
 from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
+
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="main.yaml")
 def main(cfg: DictConfig):
     hydra_cfg = HydraConfig.get()
     print(OmegaConf.to_yaml(hydra_cfg))
-    
+
+
 if __name__ == "__main__":
     main()
